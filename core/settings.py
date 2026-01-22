4d9ff77dbb007528672c821955fdc8e9926f1132
sunnatila <sunnatakbarov010407@gmail.com>
2026-01-22 15:59:11 +0500

    updated settings

--- core/settings.py
+++ core/settings.py
@@ -7,17 +7,14 @@ from pathlib import Path
 env = Env()
 env.read_env('./envs/.env')
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = env.bool("DEBUG", default=False)
 
 # ALLOWED_HOSTS = ["*"]
